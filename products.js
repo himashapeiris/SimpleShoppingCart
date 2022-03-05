f6467f25210be5be1c189ebff6087ac8a7f1c3bf
himashapeiris <w1673699@my.westminster.ac.uk>
2022-03-05 23:57:34 +0530

    Step 3

--- products.js
+++ products.js
@@ -1,28 +1,42 @@
 const products = [
-    { id: 1, name: "Shower Gel", price: 49.99 },
-    { id: 2, name: "Deodrant", price: 99.99 },
-    { id: 3, name: "Day cream", price: 9.99 },
-    { id: 4, name: "Night cream", price: 19.99 }
-  ];
-  
-  const productList = [];
-  let finalCart = {
-    productList: [],
-    totalPrice: 0
-  };
-  
-  //let productObj = {} ;
-  
-  function addProduct(productName, quantity) {
-    for (var x = 0; x < products.length; x++) {
-      if (productName === products[x].name) {
+  { id: 1, name: "Shower Gel", price: 49.99 },
+  { id: 2, name: "Deodrant", price: 99.99 },
+  { id: 3, name: "Day cream", price: 9.99 },
+  { id: 4, name: "Night cream", price: 19.99 }
+];
+
+const productList = [];
+let finalCart = {
+  productList: [],
+  totalPrice: 0
+};
+
+//let productObj = {} ;
+
+function addProduct(productName, quantity) {
+  for (var x = 0; x < products.length; x++) {
+    if (productName === products[x].name) {
+      if (productList.length != 0) {
+        console.log("Inside the if loop");
+        console.log(productList);
         if (productList.length != 0) {
-          if (productList[x].productName === productName) {
-            productList[x].productQuantity =
-              productList[x].productQuantity + quantity;
-            break;
+          const index = productList.findIndex(
+            item => item.productName === productName
+          );
+          if (index != -1) {
+            //if(vendors.find((item)=>item.Name==='Magenic'))
+            //if (productList[x].productName === productName) {
+            productList[index].productQuantity =
+              productList[index].productQuantity + quantity;
+          } else {
+            let productObj = {};
+            productObj.productName = productName;
+            productObj.productPrice = products[x].price;
+            productObj.productQuantity = quantity;
+            productList.push(productObj);
           }
         }
+      } else {
         let productObj = {};
         productObj.productName = productName;
         productObj.productPrice = products[x].price;
@@ -30,33 +44,45 @@ const products = [
         productList.push(productObj);
       }
     }
-    finalCart.productList = [...productList];
-    finalCart.totalPrice = calculateCartTotal(productList);
-    // var total = calculateCartTotal(productList);
-    //console.log("Cart Total is " + total);
-    console.log(productList);
-    console.log(JSON.stringify(finalCart));
-    return finalCart;
   }
-  
-  function calculateCartTotal(productList) {
-    var totalProductCost = 0.0;
-    var cartTotal = 0.0;
-    for (var i = 0; i < productList.length; i++) {
-      totalProductCost =
-        productList[i].productPrice * productList[i].productQuantity;
-      cartTotal = cartTotal + totalProductCost;
-    }
-  
-    return cartTotal.toFixed(2);
+  // productList.push(productObj);
+  console.log(productList);
+  finalCart.productList = [...productList];
+  finalCart.totalPrice = calculateCartTotal(productList).toFixed(2);
+  // var total = calculateCartTotal(productList);
+  //console.log("Cart Total is " + total);
+  console.log(productList);
+  console.log(JSON.stringify(finalCart));
+  return finalCart;
+}
+
+function calculateCartTotal(productList) {
+  var totalProductCost = 0.0;
+  var cartTotal = 0.0;
+  for (var i = 0; i < productList.length; i++) {
+    totalProductCost =
+      productList[i].productPrice * productList[i].productQuantity;
+    cartTotal = cartTotal + totalProductCost;
   }
-  
-  // console.log(cart);
-  
-  // //console.log(products[0].name);
-  // function addProduct(a,b){
-  //     return a+b;
-  // }
-  
-  module.exports = { addProduct, calculateCartTotal };
-  
+ // return cartTotal;
+  var salesTax = addSalesTax(cartTotal);
+  var totalAfterTax = cartTotal + salesTax;
+  return totalAfterTax;
+}
+
+function addSalesTax(cartTotal) {
+  const tax = 12.5;
+  //var roundedCartTotal = Math.round(cartTotal * 100) / 100;
+  //var salesTax = (roundedCartTotal * tax) / 100;
+  var salesTax = (cartTotal * tax) / 100;
+  return salesTax;
+}
+
+// console.log(cart);
+
+// //console.log(products[0].name);
+// function addProduct(a,b){
+//     return a+b;
+// }
+
+module.exports = { addProduct, calculateCartTotal, addSalesTax };
--- products.test.js
+++ products.test.js
@@ -29,6 +29,31 @@ const testProductObj2 = {
   ],
   totalPrice: "399.92"
 };
+const testProductObj4 = {
+  productList: [
+    {
+      productName: "Shower Gel",
+      productPrice: 49.99,
+      productQuantity: 2
+    }
+  ],
+  totalPrice: "112.48"
+};
+const testProductObj3 = {
+  productList: [
+    {
+      productName: "Shower Gel",
+      productPrice: 49.99,
+      productQuantity: 2
+    },
+    {
+      productName: "Deodrant",
+      productPrice: 99.99,
+      productQuantity: 2
+    }
+  ],
+  totalPrice: "337.45"
+};
 
 const testCart = [
   {
@@ -40,16 +65,30 @@ const testCart = [
 
 //unit tests
 
-test("Add products to the shopping cart \n The user adds 5 Shower Gels to the shopping cart", () => {
-  expect(cartModule.addProduct("Shower Gel", 5)).toMatchObject(testProductObj1);
+// Step 1
+
+// test("Add products to the shopping cart \n The user adds 5 Shower Gels to the shopping cart", () => {
+//   expect(cartModule.addProduct("Shower Gel", 5)).toMatchObject(testProductObj1);
+// });
+
+//Step 2 for this step you should uncomment the line 67 in products.js and comment the lines 68,69,70,
+
+// test("Add additional products of the same type to the shopping cart. \n And then adds another 3 Shower Gel to the shopping cart", () => {
+//   expect(cartModule.addProduct("Shower Gel", 3)).toMatchObject(testProductObj2);
+// });
+
+// test("Finding the shopping cart's total price", () => {
+//   expect(cartModule.calculateCartTotal(testCart)).toBe("249.95");
+// });
+
+test("Add products to the shopping cart", () => {
+  expect(cartModule.addProduct("Shower Gel", 2)).toMatchObject(testProductObj4);
 });
 
-test("Add additional products of the same type to the shopping cart. \n And then adds another 3 Shower Gel to the shopping cart", () => {
-  expect(cartModule.addProduct("Shower Gel", 3)).toMatchObject(testProductObj2);
+test("Add products to the shopping cart", () => {
+  expect(cartModule.addProduct("Deodrant", 2)).toMatchObject(testProductObj3);
 });
 
-test("Finding the shopping cart's total price", () => {
-  expect(cartModule.calculateCartTotal(testCart)).toBe("249.95");
+test("Calculating Sales Tax", () => {
+  expect(cartModule.addSalesTax(100.0)).toBe(12.5);
 });
-
-//test()
